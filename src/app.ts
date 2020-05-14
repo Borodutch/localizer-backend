259f7a3367da7eb6d197bc8ae4b81a7f91632564
backmeupplz <backmeupplz@gmail.com>
2020-05-13 19:11:14 -0700

    add port to .env

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,6 @@ app.use(cors({ origin: '*' }))
 app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
-app.listen(1337)
+app.listen(process.env.PORT || 1337)
 
 console.log('Koa application is up and running on port 1337')
